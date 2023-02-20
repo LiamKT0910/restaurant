25c944b1bcfce1149219f916a9cb258063390f3f
LiamKT0910 <bigboy09101994@gmail.com>
2023-02-20 14:32:32 -0600

    new chef commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+
+import "./Chef.css";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef’s Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="app__chef-content_quote-under">
+          <p className="p__opensans">
+            Hic, culpa! Reiciendis dolorem dicta vel fugit fuga, minima
+            exercitationem maxime quibusdam corporis libero eum est sint autem
+            molestias nisi quas optio, magni at enim.
+          </p>
+        </div>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
